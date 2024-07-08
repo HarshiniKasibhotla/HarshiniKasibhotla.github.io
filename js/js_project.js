e3be8253c7c159bc37521f40e3c3f870fe29ec5c
Harshini Kasibhotla <harshini.kasibhotla@Harshinis-MacBook-Air.local>
2024-07-07 18:42:19 -0700

    update profile

--- js/js_project.js
+++ js/js_project.js
@@ -22,3 +22,29 @@ $closeBtnsArr.forEach(function($btn) {
   });
 });
 
+function openProject(evt, projectName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    tabcontent[i].classList.remove("active");
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(projectName).style.display = "block";
+  document.getElementById(projectName).classList.add("active");
+  evt.currentTarget.className += " active";
+}
+
+// Open the first tab by default
+document.getElementsByClassName("tablinks")[0].click();
+
